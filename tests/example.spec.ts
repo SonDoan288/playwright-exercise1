88dd19c23cec14eaa45e1bf4e7ee7171375e92a1
“levi” <levitest28@gmail.com>
2025-07-17 01:00:52 +0700

    Initial commit

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,21 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://material.playwrightvn.com/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  // Expect a title "to contain" a text.
+  await expect(page.getByText('Tài liệu học automation test')).toBeVisible();
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://material.playwrightvn.com/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  // Click the link with text "Bài học 1: Register Page"
+  await page.locator('text=Bài học 1: Register Page').click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // Create a variable and find all heading in page, find the heading with correct text 
+  const heading = page.locator('h1, h2', { hasText: 'User Registration' });
+
+  // Expects page to have a heading with the name of User Registration.
+  await expect(heading).toHaveText('User Registration');
 });
